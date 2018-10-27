83cee7ce97faa36ad00aad164a5bc13eb57e229f
mussaiin <160107015@stu.sdu.edu.kz>
2018-10-27 11:28:59 +0600

    [refactor] documentation

--- builder/builder.py
+++ builder/builder.py
@@ -1,6 +1,55 @@
+"""
+    Builder Pattern
+    is a unique design pattern which helps in building complex
+    object using simple objects and uses an algorithmic approach.
+    This design pattern comes under the category of creational pattern.
+    In this design pattern, a builder class builds the final object in
+    step-by-step procedure. This builder is independent of other objects.
+    Designed to solve the problem of the Telescoping Constructor anti-pattern.
+
+    Telescoping Constructor example:
+        public construct_building(floor, size)
+
+    Builder Pattern Example:
+        builder = SomeBuilder()
+        building.build_floor()
+        building.build_size()
+        construct(builder)
+
+    In simple words:
+        Imagine a character generator for a role playing game.
+        The easiest option is to let computer create the character for you.
+        But if you want to select the character details like profession,
+        gender, hair color etc. the character generation becomes a step-by-step
+        process that completes when all the selections are ready.
+"""
+
 # Abstract Building
 class Building(object):
+    """
+        Abstract builder class.
+
+        Attributes:
+
+            floor: int  - count of a floors in a building
+
+            size: string - size of a building
+
+        Functions:
+
+            build_floor():
+                Here we set the floor count of a building
+
+            build_size():
+                Here we set the size of a building
+
+        :returns:
+            A string in format "Floor: {Floor count} | Size {House Size}"
+    """
+
     def __init__(self):
+        self.floor = None
+        self.size = None
         self.build_floor()
         self.build_size()
 
@@ -16,22 +65,42 @@ class Building(object):
 
 # Concrete Buildings
 class House(Building):
+    """
+        This is the concrete builder class.
+        It inherits the attributes of Building class and then sets them.
+        We have set the floor of building as 1 and size as "Small"
+    """
+
     def build_floor(self):
-        self.floor = 'One'
+        self.floor = 1
 
     def build_size(self):
         self.size = 'Small'
 
 
-class Flat(Building):
+class Apartment(Building):
+    """
+            This is the concrete builder class.
+            It inherits the attributes of Building class and then sets them.
+            We have set the floor of building as 1 and size as "Small"
+    """
     def build_floor(self):
-        self.floor = 'More than One'
+        self.floor = 3
 
     def build_size(self):
         self.size = 'Big'
 
 
 def construct_building(cls):
+    """
+        This function creates some building, then set its floor, size and returns it.
+
+        :param cls:
+            This parameter gets the name of some class and then initializes it.
+
+        :return:
+            building: Building() - concrete building.
+    """
     building = cls()
     building.build_floor()
     building.build_size()
@@ -41,5 +110,5 @@ def construct_building(cls):
 if __name__ == "__main__":
     house = House()
     print(house)
-    flat = construct_building(Flat)
+    flat = construct_building(Apartment)
     print(flat)
--- builder/tests/test_builder.py
+++ builder/tests/test_builder.py
@@ -1,7 +1,27 @@
 from unittest import TestCase
 
-
 class BuilderTest(TestCase):
     """
+        Test Methods
+
+        Method	                   |     Checks that
+        --------------------------------------------------
+        assertEqual(a, b)	       |     a == b
+        assertNotEqual(a, b)	   |     a != b
+        assertTrue(x)	           |     bool(x) is True
+        assertFalse(x)	           |     bool(x) is False
+        assertIs(a, b)	           |     a is b
+        assertIsNot(a, b)          |     a is not b
+        assertIsNone(x)	           |     x is None
+        assertIsNotNone(x)	       |     x is not None
+        assertIn(a, b)	           |     a in b
+        assertNotIn(a, b)	       |     a not in b
+        assertIsInstance(a, b)	   |     isinstance(a, b)
+        assertNotIsInstance(a, b)  |     not isinstance(a, b)
 
     """
+
+    def test_builder(self):
+        pass
+
+
