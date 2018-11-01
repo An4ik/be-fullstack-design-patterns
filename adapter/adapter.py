cab8de2f01740fd5ae3f9e9b6e898116e2f25f15
An4ik <akhmetov.anuar09@gmail.com>
2018-11-01 22:34:30 +0600

    a little fixups

--- adapter/adapter.py
+++ adapter/adapter.py
@@ -6,12 +6,13 @@
     integrated due to their incompatible interfaces.
 """
 
+
 class MediaFile:
     """
         Class is used to represent ordinary media file.
     """
+
     def __init__(self, file):
-        
         """
             :param file:
             * type: str.
@@ -23,18 +24,17 @@ class MediaFile:
             * _file_name.
             * _file_extension.
         """
-        
+
         pass
 
+
 class MediaPlayer:
-    
     """
         Class is used to represent MediaPlayer interface, which must be implemented by child classes.
         Class has abstract method play(), which have to be implemented.
     """
-    
+
     def __init__(self, mediaFile):
-        
         """
             :param mediaFile:
             * type: MediaFile.
@@ -43,26 +43,24 @@ class MediaPlayer:
             Initialize class with 'mediafile' argument as MediaPlayer(mediafile).
             Set argument to '_playing_file' attribute.
         """
-        
+
         pass
-    
+
     def play(self):
-        
         """
             Abstract method.
             Raise NotImplementedError with message.
         """
         pass
 
+
 class AdvancedMediaPlayer:
-    
     """
         Class is used to represent AdvancedMediaPlayer interface, which must be implemented by child classes.
         Class has abstract methods play_vlc() and play_mp4(), which have to be implemented.
     """
-    
+
     def __init__(self, mediaFile):
-        
         """
             :param mediaFile:
             * type: MediaFile.
@@ -72,38 +70,33 @@ class AdvancedMediaPlayer:
             Set argument to '_playing_file' attribute.
             
         """
-        
+
         pass
-    
+
     def play_vlc(self):
-        
         """
             Abstract method.
             Raise NotImplementedError with message.
         """
-        
+
         pass
-    
+
     def play_mp4(self):
-        
         """
             Abstract method.
             Raise NotImplementedError with message.
         """
-        
+
         pass
 
 
 class MP4Player(AdvancedMediaPlayer):
-    
     """
         Child class, which is inherited from AdvancedMediaPlayer interface, and implements play_mp4() method.
         
     """
-    
-    
+
     def play_mp4(self):
-        
         """
             rtype: String.
             return: The current playing mediafile in 'mp4' format, otherwise raise ExceptionError with message.
@@ -111,15 +104,14 @@ class MP4Player(AdvancedMediaPlayer):
         """
         pass
 
+
 class VLCPlayer(AdvancedMediaPlayer):
-    
     """
         Child class, which is inherited from AdvancedMediaPlayer interface, and implements play_vlc() method.
         
     """
-    
+
     def play_vlc(self):
-        
         """
             rtype: String.
             return: The current playing mediafile in 'vlc' format, otherwise raise ExceptionError with message.
@@ -129,13 +121,11 @@ class VLCPlayer(AdvancedMediaPlayer):
 
 
 class MediaAdapter(MediaPlayer):
-    
     """
         Class is used to represent MediaAdapter adapter, which is inherited from MediaPlayer interface, and implements play() method.
     """
-    
+
     def __init__(self, mediaFile):
-        
         """
             :param mediaFile:
             * type: MediaFile.
@@ -146,11 +136,10 @@ class MediaAdapter(MediaPlayer):
             Initialize '_advancedMediaPlayer' attribute and set the value as a 'None'.
             
         """
-        
+
         pass
-    
+
     def play(self):
-        
         """
             rtype: String.
             return: 'play_vlc()' or 'play_mp4()' functions of AdvancedMediaPlayer interface.
@@ -160,19 +149,17 @@ class MediaAdapter(MediaPlayer):
             * VLCPlayer("Hello.vlc"), if format of mediafile is 'vlc', or,
             * MP4Player("Hello.mp4"), if format of mediafile is 'mp4'.
         """
-        
+
         pass
 
 
 class UniversalPlayer(MediaPlayer):
-    
     """
         Class is used to represent UniversalPlayer interface, which is inherited from MediaPlayer interface, and implements play() method.
         
     """
-    
+
     def __init__(self, mediaFile):
-        
         """
             :param mediaFile:
             * type: MediaFile.
@@ -182,11 +169,10 @@ class UniversalPlayer(MediaPlayer):
             Set argument to '_playing_file' attribute.
             Initialize '_mediaAdapter' attribute and set the value as a 'None'.
         """
-        
+
         pass
-    
+
     def play(self):
-        
         """
             rtype: String.
             return: current playing mediafile in format 'mp3' or using adapter in format 'vlc' or 'mp4'.
@@ -195,6 +181,5 @@ class UniversalPlayer(MediaPlayer):
             Set argument to '_mediaAdapter' attribute as MediaAdapter("Hello.vlc") or as MediaAdapter("Hello.mp4"),
             if format of mediafile is 'vlc' or 'mp4' respectively.
         """
-        
-        pass
 
+        pass
